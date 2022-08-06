fc96182a44039281666388345e658f1a80848a2a
Ayush Parasar <parasarayush@gmail.com>
2022-08-06 22:38:18 +0530

    Updated to bootstrap 5.2 on index page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,23 @@
 // Carousel Auto-Cycle
-  $(document).ready(function() {
-    $('.carousel').carousel({
-      interval: 6000
-    })
-  });
+$(document).ready(function () {
+  $('.carousel').carousel({
+    interval: 6000
+  })
+});
 
-  $('.multi-item-carousel').carousel({
-    interval: false
-  });
-  
-  // for every slide in carousel, copy the next slide's item in the slide.
-  // Do the same for the next, next item.
-  $('.multi-item-carousel .item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-    
-    if (next.next().length>0) {
-      next.next().children(':first-child').clone().appendTo($(this));
-    } else {
-      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-    }
-  });
+let items = document.querySelectorAll('#certificationsCarousel .carousel-item')
 
-  
+items.forEach((el) => {
+  const minPerSlide = 3
+  let next = el.nextElementSibling
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0]
+    }
+    let cloneChild = next.cloneNode(true)
+    el.appendChild(cloneChild.children[0])
+    next = next.nextElementSibling
+  }
+});
+
